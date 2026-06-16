d383e8da1d109c4fc46141a910fc7f2db8d0def0
Zakaria Abdullahi <zakariaabdullahi@Zakarias-MacBook-Pro.local>
2026-06-15 17:37:18 -0700

    edoras shell up

--- xsh.h
+++ xsh.h
@@ -2,27 +2,40 @@
 // Created by Zakaria Abdullahi on 6/15/26.
 //
 
-#ifndef CS480_ASSIGNMENT_2_Z_XSH_H
-#define CS480_ASSIGNMENT_2_Z_XSH_H
+#ifndef XSH_H
 
-#endif //CS480_ASSIGNMENT_2_Z_XSH_H
-#include <iostream>
+#define XSH_H
+
 #include <string>
-#include <vector>
-using namespace std;
-
-struct Command {
-    string program;
-    string argument;
-    bool hasArgument = false;
-};
-
-struct CommandLine {
-    vector<Command> commands;
-    bool hasPipe = false;
-    bool valid = true;
-};
 
 void printPrompt();
-string readInput();
 
+std::string readInput();
+
+#endif
+
+//
+// #ifndef CS480_ASSIGNMENT_2_Z_XSH_H
+// #define CS480_ASSIGNMENT_2_Z_XSH_H
+//
+// #endif //CS480_ASSIGNMENT_2_Z_XSH_H
+// #include <iostream>
+// #include <string>
+// #include <vector>
+// using namespace std;
+//
+// struct Command {
+//     string program;
+//     string argument;
+//     bool hasArgument = false;
+// };
+//
+// struct CommandLine {
+//     vector<Command> commands;
+//     bool hasPipe = false;
+//     bool valid = true;
+// };
+//
+// void printPrompt();
+// string readInput();
+//
