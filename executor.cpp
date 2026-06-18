4b843fe159564342a5864176a88793d85ce16e1f
Zakaria Abdullahi <zakariaabdullahi@Zakarias-MacBook-Pro.local>
2026-06-18 15:11:34 -0700

    execlp

--- executor.cpp
+++ executor.cpp
@@ -2,16 +2,50 @@
 // Created by Zakaria Abdullahi on 6/15/26.
 //
 #include "xsh.h"
+#include <unistd.h>
+#include <sys/wait.h>
+#include <cstdlib>
+
+
+void executePipedCommand(const vector<Command>& commands) {
+    int numCommands = commands.size();
+    int numPipes = numCommands - 1;
+}
+
 
 void executeCommand(const CommandLine& commandLine) {
     if (commandLine.commands.empty()) {
-        cout << "Invalid command... " << endl;
+        return;
+    }
+    if (commandLine.hasPipe) {
+        executePipedCommand(commandLine.commands);
+    }
+
+    Command command = commandLine.commands[0];
+
+    pid_t pid = fork();
+
+    if (pid == -1) {
+        perror("fork failed");
+        return;
+    }
+    if (pid == 0) {
+        if (command.hasArgument) {
+            execlp(command.program.c_str(),
+                command.program.c_str(),
+                command.argument.c_str(),
+                nullptr);
+        }
+        else {
+            execlp(command.program.c_str(),
+                command.program.c_str(),
+                nullptr);
+        }
+        perror("exec failed");
+        exit(1);
     }
     else {
-        cout << "Number of commands: " << commandLine.commands.size() << endl;
-        for (int i = 0; i < commandLine.commands.size(); i++) {
-            cout << "Command: " << i + 1 << ": "
-                 << commandLine.commands[i].program << endl;
-        }
+        waitpid(pid, nullptr, 0);
     }
 }
+
